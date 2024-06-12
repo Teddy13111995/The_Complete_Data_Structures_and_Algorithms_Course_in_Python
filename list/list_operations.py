c3636b7e2798662b9f557ec391d3442144006cab
DELL <2022proj037@hyderabad.bits-pilani.ac.in>
2024-06-13 01:33:37 +0530

    init

---
+++ list/list_operations.py
@@ -0,0 +1,16 @@
+my_list = [1, 2, 3, 4, 5, 6]
+print(my_list)
+my_list.insert(2, 10)
+print(my_list)
+my_list.append(7)
+my_list_2 = [11, 12, 13]
+my_list.extend(my_list_2)
+print(my_list)
+
+
+# list slicing
+my__list = ['a','b','c']
+print(my__list[0:2])
+print(my__list[1:])
+
+# deletion
