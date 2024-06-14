a52edadd2b99d1e98b54648b404daaf4a0b089ec
DELL <2022proj037@hyderabad.bits-pilani.ac.in>
2024-06-14 12:51:04 +0530

    init

--- list/list_operations.py
+++ list/list_operations.py
@@ -1,4 +1,4 @@
-def insertInList():
+def insert_in_list():
     my_list = [1, 2, 3, 4, 5, 6]
     print(my_list)
     my_list.insert(2, 10)
@@ -10,14 +10,14 @@ def insertInList():
 
 
 # list slicing
-def slicingList():
+def slicing_list():
     my__list = ['a', 'b', 'c']
     print(my__list[0:2])
     print(my__list[1:])
 
 
 # deletion
-def deletionInList():
+def deletion_in_list():
     my_list_3 = ['a', 'b', 'c', 'd', 'e']
     my_list_3.pop(2)
     print(my_list_3)
@@ -27,22 +27,47 @@ def deletionInList():
     print(my_list_3)
 
 
-def searchingInList():
+def searching_in_list():
     my_list = [10, 20, 30, 40, 50, 60, 70, 80]
     target = 50
     if target in my_list:
         print(f'{target} in list')
     else:
         print(f'{target} not in list')
-    print(linearSearch(my_list, target))
+    print(linear_search(my_list, target))
 
 
-def linearSearch(p_list, p_target):
+def linear_search(p_list, p_target):
     for i, value in enumerate(p_list):
         if value == p_target:
             return i
     return -1
 
 
+def list_functions():
+    # + operator - concatenate 2 list
+    list_1 = [10, 20, 30]
+    list_2 = [40, 50, 60]
+    my_list = list_1 + list_2
+    print(my_list)
+    # * operator - repeat element n times
+    a = [0]*4
+    print(a)
+
+    # len() - count number of elements
+    a = [1,2,3,4,5]
+    print(len(a))
+
+    #max() - find maximum element in list
+    print(max(a))
+    #min() - find minimum element in list
+    print(min(a))
+    #sum() - find sum of elements in a list
+    print(sum(a))
+
+def average_using_list():
+    my_list = []
+
+
 if __name__ == '__main__':
-    searchingInList()
+    list_functions()
