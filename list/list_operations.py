3bf12f1fd1daca82b08c9d8b550959bf249d5ccb
DELL <2022proj037@hyderabad.bits-pilani.ac.in>
2024-06-15 14:13:43 +0530

    init

--- list/list_operations.py
+++ list/list_operations.py
@@ -1,3 +1,6 @@
+import random
+
+
 def insert_in_list():
     my_list = [1, 2, 3, 4, 5, 6]
     print(my_list)
@@ -51,23 +54,134 @@ def list_functions():
     my_list = list_1 + list_2
     print(my_list)
     # * operator - repeat element n times
-    a = [0]*4
+    a = [0] * 4
     print(a)
 
     # len() - count number of elements
-    a = [1,2,3,4,5]
+    a = [1, 2, 3, 4, 5]
     print(len(a))
 
-    #max() - find maximum element in list
+    # max() - find maximum element in list
     print(max(a))
-    #min() - find minimum element in list
+    # min() - find minimum element in list
     print(min(a))
-    #sum() - find sum of elements in a list
+    # sum() - find sum of elements in a list
     print(sum(a))
 
+
 def average_using_list():
-    my_list = []
+    my_list = list()
+    while True:
+        inp = input('Enter a number')
+        if inp == 'done': break
+        my_list.append(int(inp))
+    average = sum(my_list) / len(my_list)
+    return average
+
+
+def list_from_string():
+    a = 'spam'
+    b = list(a)
+    print(b)
+    a = 'using split function'
+    b = list(a.split(' '))
+    print(b)
+    a = 'using-delemiter-function'
+    b = a.split('-')
+    print(b)
+
+    print('-'.join(b))
+
+
+def list_pitfall():
+    my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+    my_list = my_list.sort()  # my_list becomes None
+    my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+    my_list.append([10])  # appends list to another list
+    # make a copy
+    copied_list = my_list[:]
+    # using sorted(my_list) doesn't alter list
+
+
+def list_vs_arrays():
+    """
+    Similarities
+    1. Both data structure are mutable
+    2. Both can be indexed and iterated through
+    3. They can be both sliced
+
+    Differences
+    1. array is used for arithmetic operations
+    2. list elements can have different data types
+
+    """
+    import numpy as np
+    my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    new_list = np.array(my_list)
+
+    print(new_list / 2)
+    print(my_list / 2)
+    pass
+
+
+def tc_sc_list():
+    """
+    op                              tc      sc
+    creating an empty list          O(1)    O(1)
+    creating a list with elements   O(n)    O(n)
+    inserting a value in list       O(n)    O(1)
+    traversing a list               O(n)    O(1)
+    accessing a given cell          O(1)    O(1)
+    searching a given value         O(n)    O(1)
+    deleting a value from list      O(1)    O(1)
+    """
+    pass
+
+
+def list_comprehension():
+    prev_list = [1, 2, 3]
+    # new_list = [new_item for item in list]
+    new_list = [2 * x for x in prev_list]
+    print(new_list)
+
+    str_ = 'python'
+    list_char = [x for x in str_]
+    print(list_char)
+    "list comprehension can be used for list, range, string and tuple"
+
+
+def conditional_list_comprehension():
+    # new_list = [new_item for item in list if condition]
+    prev_list = [-1, 10, -20, 2, -90, 60, 45, 20]
+    new_list = [num for num in prev_list if num > 0]
+    print(new_list)
+    new_list = [num ** 2 for num in prev_list if num < 0]
+    print(new_list)
+
+    pass
+
+
+def string_list_comprehension():
+    def is_consonant(letter):
+        vowels = 'aeiou'
+        return letter.isalpha() and letter.lower() not in vowels
+
+    sentence = 'my name is srijan'
+    consonants = [letter for letter in sentence if is_consonant(letter)]
+    print(consonants)
+
+    # new_list = [ new_item if condition for item in list]
+    new_list = ['apple','banana','mango']
+    random.shuffle(new_list)
+    print(new_list)
+
+    a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(a[::2])
+
+    # a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # a[::2] = 10, 20, 30, 40, 50, 60
+    # print(a)
 
 
 if __name__ == '__main__':
-    list_functions()
+    string_list_comprehension()
